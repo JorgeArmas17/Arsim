fc1369ecd284602260c7189fbaabc0d135a9d80b
JorgeArmas <jorgearmas51@gmail.com>
2023-07-30 12:30:32 -0500

    Nuevo Login

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,17 +1,23 @@
 import 'package:arsim/components/my_button.dart';
 import 'package:arsim/components/my_textfield.dart';
 import 'package:arsim/components/square_tile.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
   LoginPage({super.key});
 
   //text editing controller
-  final userNameController  = TextEditingController();
+  final userNameController = TextEditingController();
   final passwordController = TextEditingController();
 
   //sign user in method
-  void signUserIn(){}
+  Future signUserIn() async {
+    FirebaseAuth.instance.signInWithEmailAndPassword(
+      email: userNameController.text.trim(),
+      password: passwordController.text.trim(),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,12 +25,14 @@ class LoginPage extends StatelessWidget {
       backgroundColor: Colors.grey[300],
       body: SafeArea(
         child: Center(
-          child: Column(
-            children: [
-              const SizedBox(height: 50),
+          child: Column(children: [
+            const SizedBox(height: 50),
             //logo
-            const Icon(Icons.lock,size: 100,),
-            
+            const Icon(
+              Icons.lock,
+              size: 100,
+            ),
+
             const SizedBox(height: 50),
             //welcome back, you've been mised
             Text(
@@ -34,18 +42,18 @@ class LoginPage extends StatelessWidget {
                 fontSize: 16,
               ),
             ),
-            
+
             const SizedBox(height: 25),
 
             //username textfield
             MyTextField(
-              controller:  userNameController,
-              hintText: 'Username',
+              controller: userNameController,
+              hintText: 'Email',
               obscureText: false,
             ),
 
             const SizedBox(height: 10),
-            
+
             //password textfield
             MyTextField(
               controller: passwordController,
@@ -70,54 +78,9 @@ class LoginPage extends StatelessWidget {
             ),
 
             const SizedBox(height: 25),
-  
+
             //sign in button
-            MyButton(
-              onTap: signUserIn
-            ),
-
-            const SizedBox(height: 50),
-  
-            //or continue with
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Divider(
-                      thickness: 0.3,
-                      color: Colors.grey[400],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                    child: Text(
-                      'Or Continue With',
-                      style: TextStyle(color: Colors.grey[700]),
-                      ),
-                  ),
-                  Expanded(
-                    child: Divider(
-                      thickness: 0.3,
-                      color: Colors.grey[400],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-
-            //google + apple sign in buttons
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                //google Button
-                SquareTile(imagePath: 'lib/images/google.png'),
-                
-                SizedBox(width: 25),
-                //Apple Button
-                SquareTile(imagePath: 'lib/images/apple.png'),
-              ],
-            ),
+            MyButton(onTap: signUserIn),
 
             const SizedBox(height: 50),
 
@@ -129,8 +92,7 @@ class LoginPage extends StatelessWidget {
                 Text(
                   'Register now',
                   style: TextStyle(
-                    color: Colors.blue, fontWeight: FontWeight.bold
-                  ),
+                      color: Colors.blue, fontWeight: FontWeight.bold),
                 ),
               ],
             )
