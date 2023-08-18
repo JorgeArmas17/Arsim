b1369800572a6c1ccaacd0a78afb036e76610f8a
JorgeArmas <jorgearmas51@gmail.com>
2023-08-18 09:51:46 -0500

    Carousel Menu

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -2,6 +2,30 @@ import 'package:arsim/components/button.dart';
 import 'package:arsim/components/my_button.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+class CarouselItem {
+  final String title;
+  final String imageUrl;
+  final String description;
+
+  CarouselItem(
+      {required this.title, required this.imageUrl, required this.description});
+}
+
+List<CarouselItem> carouselItems = [
+  CarouselItem(
+    title: 'EL RODELU',
+    imageUrl: 'lib/images/sushiman.png',
+    description: 'Taste the most popular food from anywhere any time',
+  ),
+  CarouselItem(
+    title: 'DORILOKO',
+    imageUrl: 'lib/images/carne.png',
+    description: 'Tas',
+  ),
+  // Agrega más elementos del carrusel según tus necesidades
+];
 
 class IntroPage extends StatelessWidget {
   const IntroPage({super.key});
@@ -12,55 +36,65 @@ class IntroPage extends StatelessWidget {
       backgroundColor: Color.fromARGB(255, 130, 60, 55),
       body: Padding(
         padding: const EdgeInsets.all(25.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            const SizedBox(height: 25),
-            //Nombre Restaurante
-            Text(
-              "EL RODELU",
-              style:
-                  GoogleFonts.dmSerifDisplay(fontSize: 28, color: Colors.white),
-            ),
-
-            const SizedBox(height: 25),
-
-            //Icono
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image.asset('lib/images/sushiman.png'),
-            ),
-
-            //Titulo
-            Text(
-              "THE TASTE OF ECUATORIAN FOOD",
-              style:
-                  GoogleFonts.dmSerifDisplay(fontSize: 44, color: Colors.white),
-            ),
-
-            const SizedBox(height: 10),
-
-            //Subtitulo
-            Text(
-              "Taste the most pouplar food from anywhere any time",
-              style: TextStyle(
-                color: Colors.grey[300],
-                height: 2,
-              ),
-            ),
-
-            const SizedBox(height: 25),
-
-            //Boton Inicio
-            MyMainButton(
-              text: "Get Started",
-              onTap: () {
-                //ir a la pagina de menu
-                Navigator.pushNamed(context, '/menupage');
+        child: CarouselSlider(
+          options: CarouselOptions(
+            height: MediaQuery.of(context).size.height,
+            viewportFraction: 1.0,
+            autoPlay: false,
+            autoPlayInterval: Duration(seconds: 5),
+            autoPlayAnimationDuration: Duration(milliseconds: 800),
+            autoPlayCurve: Curves.fastOutSlowIn,
+          ),
+          items: carouselItems.map((item) {
+            return Builder(
+              builder: (BuildContext context) {
+                return Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      const SizedBox(height: 25),
+                      Text(
+                        item.title,
+                        style: GoogleFonts.dmSerifDisplay(
+                          fontSize: 28,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 25),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Image.asset(item.imageUrl),
+                      ),
+                      Text(
+                        "THE TASTE OF ECUATORIAN FOOD",
+                        style: GoogleFonts.dmSerifDisplay(
+                          fontSize: 44,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                        item.description,
+                        style: TextStyle(
+                          color: Colors.grey[300],
+                          height: 2,
+                        ),
+                      ),
+                      const SizedBox(height: 25),
+                      MyMainButton(
+                        text: "Get Started",
+                        onTap: () {
+                          // Navigate to the menu page
+                          Navigator.pushNamed(context, '/menupage');
+                        },
+                      ),
+                    ],
+                  ),
+                );
               },
-            )
-          ],
+            );
+          }).toList(),
         ),
       ),
     );
