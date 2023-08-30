5f52947c174efe132e8a2c0876fd9d27321ad960
alexander176 <shuguli321@gmail.com>
2023-08-29 21:04:24 -0500

    Final

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,7 +1,11 @@
 import 'package:arsim/pages/home_page.dart';
+import 'package:arsim/pages/platosform.dart';
+import 'package:arsim/pages/platosview.dart';
+import 'package:arsim/pages/redesociales.dart';
+import 'package:arsim/pages/restaurantform.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
 import 'login_or_register_page.dart';
 import 'login_page.dart';
 
@@ -16,10 +20,55 @@ class AuthPage extends StatelessWidget {
         builder: (context, snapshot) {
           //user is logged in
           if (snapshot.hasData) {
-            return HomePage();
+            return BottomNavigation();
+            ;
           } else {
             return LoginOrRegisterPage();
           }
+        },
+      ),
+    );
+  }
+}
+
+class BottomNavigation extends StatefulWidget {
+  const BottomNavigation({Key? key}) : super(key: key);
+
+  @override
+  _BottomNavigationState createState() => _BottomNavigationState();
+}
+
+class _BottomNavigationState extends State<BottomNavigation> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = [
+    RestauranteForm(),
+    DishForm(),
+    Dish(),
+    SocialMediaPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Color(0xffD6E2EA),
+        color: Color.fromARGB(255, 108, 165, 218),
+        animationDuration: Duration(milliseconds: 300),
+        items: const [
+          Icon(Icons.restaurant, color: Color.fromARGB(255, 0, 0, 0)),
+          Icon(Icons.fastfood, color: Color.fromARGB(255, 0, 0, 0)),
+          Icon(Icons.mode_edit_outline_rounded,
+              color: Color.fromARGB(255, 0, 0, 0)),
+          Icon(Icons.alternate_email_rounded,
+              color: Color.fromARGB(255, 0, 0, 0)),
+        ],
+        index: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
         },
       ),
     );
